5cac6a8bb6772cecd4c1a089ec34a72a7c85d26c
Katerina190 <fatcat2019@gmail.com>
2024-09-30 15:28:35 +0300

    initial commit

---
+++ FixYourTime/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FixYourTime
+//
+//  Created by Ekaterina Sedova on 30.09.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FixYourTime/FixYourTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FixYourTimeApp.swift
+//  FixYourTime
+//
+//  Created by Ekaterina Sedova on 30.09.24.
+//
+
+import SwiftUI
+
+@main
+struct FixYourTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
